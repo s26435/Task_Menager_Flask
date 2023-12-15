fd2eef56902781ef93733d9d1eb5447cfbcf5313
s26435 <s26435@pjwstk.edu.pl>
2023-12-15 15:45:40 +0100

    feat: 1.0 app full-func - creating and updating tasks - deleting tasks - UI, css - database

--- app.py
+++ app.py
@@ -1,12 +1,64 @@
-from flask import Flask
+from flask import Flask, render_template, request, redirect
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
+db = SQLAlchemy(app)
 
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+class ToDo(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    date_created = db.Column(db.DateTime, default=datetime.utcnow())
 
+    def __repr__(self):
+        return '<Task %r>' % self.id
+
+
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        task_content = request.form['content']
+        new_task = ToDo(content=task_content)
+        try:
+            db.session.add(new_task)
+            db.session.commit()
+            return redirect("/")
+        except:
+            return 'Adding task to database failed'
+    else:
+        tasks = ToDo.query.order_by(ToDo.date_created).all()
+        return render_template('index.html', tasks=tasks)
+
+
+@app.route('/delete/<int:id>')
+def deleting(id):
+    task_to_delete = ToDo.query.get_or_404(id)
+    try:
+        db.session.delete(task_to_delete)
+        db.session.commit()
+        return redirect('/')
+    except:
+        return 'There was problem deleting that task'
+
+
+@app.route('/update/<int:id>', methods=['GET', 'POST'])
+def update(id):
+    task = ToDo.query.get_or_404(id)
+    if request.method == 'POST':
+        task.content = request.form['content']
+        try:
+            db.session.commit()
+            return redirect('/')
+        except:
+            return "Error during updating task"
+    else:
+        return render_template('update.html', task=task)
+
+
+with app.app_context():
+    db.create_all()
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
